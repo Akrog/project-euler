e2e38f53e536a98f229b67c3775ef9e3758240fd
akrog <gorka@eguileor.com>
2014-08-09 10:47:06 +0100

    014: Better solution, 1/4 of the time

--- 014.py
+++ 014.py
@@ -39,25 +39,77 @@ print __doc__
 
 maxCol = 1000000
 
+
+#-----------------
+#Trivial solution: Very slow
+#-----------------
 # We only need to check numbers we haven't passed through in previous Collatz
 # sequences.
 # So we use a set to keep track of what numbers we have already checked.
+#
+#pending = set(xrange(2,maxCol))
+#max = (0,0)
+#while pending:
+#    # Get one of the pending elements
+#    i = pending.pop()
+#
+#    # Construct its Collatz list
+#    c = list(Collatz(i))
+#
+#    # See if it's better than current best
+#    if len(c) > max[0]:
+#        max = (len(c), i)
+#
+#    # Remove all elements we've visited from the pending list
+#    pending.difference_update(c)
 
-pending = set(xrange(2,maxCol))
+#----------------
+#Better solution: Executes in less than 1/4 of the time of the Trivial solution
+#----------------
+# We start from the higher numbers we have to check.
+# We keep track of the lengths of every Collatz sequence.
+# Wen we reach a Collatz sequence we've already examined we use its stored
+# length to calculate the current squence length.
 
-max = (0,0)
-while pending:
-    # Get one of the pending elements
-    i = pending.pop()
+import itertools as it
 
-    # Construct its Collatz list
-    c = list(Collatz(i))
+nextCollatz = lambda n: n >> 1 if n&1 == 0 else 3*n + 1
 
-    # See if it's better than current best
-    if len(c) > max[0]:
-        max = (len(c), i)
+lengths = {1:1}
+max = (1, 1)
 
-    # Remove all elements we've visited from the pending list
-    pending.difference_update(c)
+pending = maxCol-1
+
+for i in xrange(maxCol-1, 1, -1):
+    # If we already have that Collatz sequence's length
+    if i in lengths:
+        continue
+
+    # Get it's Collatz sequence until we reach a sequence we've already done
+    new_path = list(it.takewhile(lambda c: c not in lengths, Collatz(i)))
+
+    # We get the Collatz number that made us stop
+    stop = nextCollatz(new_path[-1])
+    length = lengths[stop] + len(new_path)
+
+    # We store the lengths of the partial sequences we've traversed, but only if
+    # they are within those we are going to check. We don't include those that
+    # go beyond maxCol. It is faster this way.
+    new_lengths = {k:l
+                   for (k,l) in it.izip(new_path, it.count(length,-1))
+                   if k < maxCol}
+
+    # Update the lengths' dictionary
+    lengths.update(new_lengths)
+
+    # If this is the new max we store it
+    if length > max[0]:
+        max = (length,i)
+
+    # Check if we have already checked all Collatz sequences
+    pending -= len(new_path)
+    if pending == 0:
+        break
+
 
 print "The longest chain is produced by starting number {} and consists of {} elements".format(max[1], max[0])
